814880618cd74654221c429a3db2eba63b75454f
Jorge Gómez <gomezjorge.exequiel@gmail.com>
2015-12-11 16:29:52 -0300

    Merge.

--- main.hs
+++ main.hs
@@ -1,8 +1,16 @@
 import Graphics.UI.Gtk
 
+hello :: (ButtonClass o) => o -> IO ()
+hello b = set b [buttonLabel := "Hello World"]
+
 main :: IO ()
 main = do
   initGUI
   window <- windowNew
+  button <- buttonNew
+  set window [windowDefaultWidth := 800, windowDefaultHeight := 400,
+              containerChild := button, containerBorderWidth := 10]
+  onClicked button (hello button)
+  onDestroy window mainQuit
   widgetShowAll window
   mainGUI
