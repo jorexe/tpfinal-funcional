832e4918eb5f10ad9c6da93e37655aab2758ffce
Jorge Gómez <gomezjorge.exequiel@gmail.com>
2015-12-11 17:01:02 -0300

    textView example.

--- main2.hs
+++ main2.hs
@@ -4,7 +4,7 @@ main :: IO ()
 main= do
   initGUI
   window <- windowNew
-  set window [windowTitle := "Text Entry", containerBorderWidth := 10]
+  set window [windowTitle := "Text Editor", containerBorderWidth := 0,windowDefaultWidth := 800, windowDefaultHeight := 400]
 
   vb <- vBoxNew False 0
   containerAdd window vb
@@ -12,17 +12,17 @@ main= do
   hb <- hBoxNew False 0
   boxPackStart vb hb PackNatural 0
 
-  txtfield <- entryNew
-  boxPackStart hb txtfield PackNatural 5
-  button <- buttonNewFromStock stockInfo
-  boxPackStart hb button PackNatural 0
+  txtfield <- textViewNew
+  boxPackStart hb txtfield PackGrow 2
+  --button <- buttonNewFromStock stockInfo
+  --boxPackStart hb button PackNatural 0
 
   txtstack <- statusbarNew
   boxPackStart vb txtstack PackNatural 0
   id <- statusbarGetContextId txtstack "Line"
 
   widgetShowAll window
-  widgetSetSensitivity button False
+  --widgetSetSensitivity button False
 
   --onEntryActivate txtfield (saveText txtfield button txtstack id)
   --onPressed button (statusbarPop txtstack id)
