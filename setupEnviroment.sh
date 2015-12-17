c73bb1bcb46040569788e8283295fc6284c6000b
fbejaran <fbejaran@itba.edu.ar>
2015-12-17 18:17:19 -0300

    ejemplo basico de spellcheck en probando8.hs

--- setupEnviroment.sh
+++ setupEnviroment.sh
@@ -2,3 +2,11 @@
 sudo apt-get install ghc
 sudo apt-get install libghc-gtk-dev
 
+#programas necesarios para el corrector ortografico Haspell.
+sudo apt-get install aspell
+sudo apt-get install aspell-es
+sudo apt-get install libaspell-dev
+sudo apt-get install cabal-install
+cabal update
+cabal install haspell
+
